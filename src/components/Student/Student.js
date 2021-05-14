aa046084c9c26a66559e0ac569954f9f2b4847f1
Nathaniel Shirts <nshirts7@gmail.com>
2021-05-13 20:18:42 -0600

    finished need to do the black diamond

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class Student extends Component {
   constructor() {
-    super()
+    super();
 
+    this.state = {
+      studentInfo: {},
+    };
   }
 
+  componentDidMount = () => {
+    const ID_GOES_HERE = this.props.match.params.id;
+    axios
+      .get(`http://localhost:3005/students/${ID_GOES_HERE}`)
+      .then((res) => {
+        this.setState({
+          studentInfo: res.data,
+        });
+      })
+      .catch((err) => console.log(err));
+  };
+
   render() {
+    const { first_name, last_name, grade, email } = this.state.studentInfo;
     return (
       <div className="box">
         <h1>Student</h1>
+        <h1>
+          {first_name} {last_name}
+        </h1>
+        <h3>Grade: {grade}</h3>
+        <h3>Email: {email}</h3>
       </div>
-    )
+    );
   }
 }
